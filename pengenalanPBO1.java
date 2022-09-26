cebf3df6679f5767db6d099002edbd52a9bb7212
AldiansyahRamdhan <114386794+AldiansyahRamdhan@users.noreply.github.com>
2022-09-26 15:12:40 +0700

    Add files via upload

---
+++ pengenalanPBO1.java
@@ -0,0 +1,16 @@
+//Nama : Aldiansyah Ramadhan
+//NIM : 20210040070
+//Kelas : TI21E
+
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class pengenalanPBO1 {
+    public static void main(String[] args) {
+        String input;
+        input = JOptionPane.showInputDialog("Anda Sedang belajar apa?");
+        JOptionPane.showMessageDialog(null, "Belajar "+input+" sangat mudah","Message",JOptionPane.INFORMATION_MESSAGE);
+        
+        
+    }
+}
